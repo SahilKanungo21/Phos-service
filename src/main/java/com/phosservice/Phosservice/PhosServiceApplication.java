4878dbbfdf473b26d78202360e49a0dccd6dce86
Sahil Kanungo <kanungosahil123@gmail.com>
2023-01-28 11:56:19 +0530

    Initial commit

---
+++ src/main/java/com/phosservice/Phosservice/PhosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.phosservice.Phosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhosServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phosservice/Phosservice/PhosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phosservice.Phosservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
